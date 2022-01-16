9733d0a4a7d861f9e7f5df115dc4c905a3226449
Terell Luna <terell.luna@yahoo.com>
2022-01-15 22:30:09 -0600

    finalized nav bar

--- delta-luna/src/NavBar/navbar.js
+++ delta-luna/src/NavBar/navbar.js
@@ -1,6 +1,11 @@
+import Delta_Luna_Logo from '../images/Delta_Luna_Logo.png';
+
 const NavBar = () => {
     return <>
-        <nav className="navbar">Delta Luna</nav>
+        <nav className="navbar">
+            <img src={Delta_Luna_Logo} alt="Delta Luna Logo"/>
+            <h1>Delta Luna</h1>
+        </nav>
     </>
 }
 
